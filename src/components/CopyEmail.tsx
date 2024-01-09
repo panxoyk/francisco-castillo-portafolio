2464ef84eda2840852b16acdbe0fdb08d8132313
panxoyk <francisco.cstllhrnndz@gmail.com>
2024-01-09 02:15:34 -0300

    Added: subtitle and hover animations on links, updated button component.

--- src/components/CopyEmail.tsx
+++ src/components/CopyEmail.tsx
@@ -13,7 +13,7 @@ const CopyEmail = ({ email }: CopyEmailProps) => {
     return (
         <div>
             <button
-            className="h-8 border min-w-max text-sm rounded-full px-4 py-1 border-accent text-primary hover:scale-105 transition duration-300 ease-in flex flex-row justify-between items-center gap-2"
+            className="h-8 border min-w-max text-sm rounded-full px-4 py-1 border-accent text-primary hover:scale-110 transition duration-300 ease-in flex flex-row justify-between items-center gap-2"
             onClick={handleOnCLick}
             >
                 <img width={"20px"} src="send.svg" alt="Icono enviar mensaje" />
